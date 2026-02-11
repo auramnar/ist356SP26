6568dd11d32f483b288d88ccd7885601dc0a6af3
auramnar <auramnar@g.syr.edu>
2026-02-11 16:54:29 -0500

    soln

--- 3-data/solutions/3-1-1.py
+++ 3-data/solutions/3-1-1.py
@@ -1,3 +1,21 @@
 import pandas as pd
 import numpy as np
 
+s1 = pd.Series(data= [1,2,3,4], index=['a','b','c','d'], name='s1')
+s2 = pd.Series(data=[2.2, np.nan, 3.0, 1.5],index=['a','b','c','d'], name='s2')
+s3 = pd.Series(data=['q','q','z','z'], index=['a','b','c','d'], name='s3')
+df= pd.DataFrame(
+    {'s1':s1,
+     's2':s2,
+     's3':s3
+        
+        
+    }
+    
+    
+)
+
+print(df)
+
+twoqs =df.loc['a':'b', 's3']
+print(twoqs)
