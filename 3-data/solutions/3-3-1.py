1ac9f0aa3a0c9bf1590352ae98b329c1a649426d
auramnar <auramnar@g.syr.edu>
2026-02-19 23:41:17 -0500

    challenges

--- 3-data/solutions/3-3-1.py
+++ 3-data/solutions/3-3-1.py
@@ -1,19 +1,35 @@
-import pandas as pd
-import numpy as np
-import requests
-import streamlit as st
-
-'''for each department:
+'''
+for each department:
     #create a dataframe for that department (e.g. from the json under the department)
     #add lineage to the dataframe (e.g. add the department name)
     #add the dataframe to a list of departments
 concat the list of departments together one dataframe
 print dataframe
 '''
+import pandas as pd
+import numpy as np
+import requests
+import streamlit as st
 
 
 link ="https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/json-samples/employees-dict.json"
 response = requests.get(link)
 employees = response.json()
 st.write(employees)
+
+#f = pd.DataFrame(employees["accounting"])
+#st.dataframe(df)
+
+departments = []
+for dept_name in employees.keys():
+    dept_employees = pd.json_normalize(employees, record_path=dept_name)
+    st.dataframe(dept_employees)
+    dept_employees['dept'] = dept_name
+    departments.append(dept_employees)
+    
+combined = pd.concat(departments, ignore_index=True)
+
+st.dataframe(combined)
+    
+    
 
--- 3-data/solutions/3-3-2.py
+++ 3-data/solutions/3-3-2.py
@@ -6,8 +6,27 @@ import numpy as np
 
 
 base = "https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/minimart/"
-months = ['jan', 'feb', 'mar', 'apr']
+months = ['jan', 'feb', 'mar', 'apr'] # list of months to use in a dropdown menu
+
+st.title("Who's not Buying from MiniMart?")
+month = st.selectbox('Select Month:', months) # dropdown menu
+
+purchases = pd.read_csv(f"{base}/purchases-{month}.csv") # load file based on month selected
+st.dataframe(purchases) # display purchase
+
+# Merge purchases with customers on customet id
+# Left join to keep all customers
+# Those who did not buy will have a null order id
+# We filter for those null order
 
 customers = pd.read_csv(f"{base}/customers.csv")
-st.dataframe(customers)
+combined = pd.merge(customers, purchases, left_on='customer_id', right_on= 'customer_id', how='left')
+st.dataframe(combined)
+
+
+cols= ['customer_id', 'firstname', 'lastname'] # select colunms to display
+did_not_buy = combined["order_id"].isnull() #return boolean series where true means the customer did not buy anything
+customers_who_did_not_buy = combined[did_not_buy][cols] # filter for customers who did not buy and select only the columns we want to display
+
+st.dataframe(customers_who_did_not_buy, hide_index=True) # display customers who did not buy and exclude index
 
