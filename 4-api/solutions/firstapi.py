c7912e5820f668121fe1bc0d847271a382ace541
auramnar <auramnar@g.syr.edu>
2026-03-02 13:40:18 -0500

    update

--- 4-api/solutions/firstapi.py
+++ 4-api/solutions/firstapi.py
@@ -8,10 +8,4 @@ curl -X 'GET' \
 import requests
 
 url = 'https://cent.ischool-iot.net/api/funnyname/random'
-querystring = {'n': 3}
-response = requests.get(url, params=querystring)
-response.raise_for_status()
-names = response.json()
-for name in names:
-    print(name['first'], name['last'])
-               
+
