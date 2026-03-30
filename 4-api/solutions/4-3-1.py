a357ac4194052170bea733f1af333bbf27903c0a
auramnar <auramnar@g.syr.edu>
2026-03-30 14:02:52 -0400

    update

--- 4-api/solutions/4-3-1.py
+++ 4-api/solutions/4-3-1.py
@@ -11,28 +11,3 @@ curl -X 'POST' \
 import requests
 import streamlit as st
 
-def generate_ai_response(prompt: str) -> str:
-    params = {'temperature}
-    url = "https://cent.ischool-iot.net/api/genai/generate"
-    data = {"query": prompt}
-    headers = {"X-API-KEY": "your_api_key_here"}
-    response = requests.post(url, data= data, headers=headers) 
-    response.raise_for_status()
-    results = response.json()
-    return results
-
-
-  
-def spellcheck(text: str) -> str: 
-    prompt = f"Spellcheck the following text:"
-    prompt += text + "\n"
-    prompt += "for each misspelling provide one suggestion"
-    check = generate_ai_response(prompt)
-    return check
-    
-# MAIN PROGRAM
-st.title("Spellcheck with AI")
-text = st.text_area("Enter text to spellcheck:")
-s_check = spellcheck(text)
-st.write(s_check)
-
