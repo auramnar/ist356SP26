093054bfe1e6527481d6f4be2786024259e92e6b
auramnar <auramnar@g.syr.edu>
2026-04-01 16:58:54 -0400

    challenges

--- 4-api/fasthello.py
+++ 4-api/fasthello.py
@@ -6,7 +6,17 @@ app = FastAPI()  # Create a FastAPI instance
 def root():      # Define a function that will be called when the route is requested
     return {"message": "Hello World"} # Serializes to JSON automatically
 
+@app.get("/hi/{name}") # Define a route with a path parameter "name"
+def say_hi(name: str): # Define a function that takes the path parameter "name"
+    return {"message": f"Hi {name}"} # Return a personalized greeting message
 
 
-
-
+@app.get("/roll/{number}/d/{sides}")
+def roll_dice(number: int, sides: int):
+    import random 
+    rolls = random.randint(1, sides) * number
+    return {
+        "number_of_rolls": number,
+        "number_of_sides": sides,
+        "total of rolls": rolls
+    }
--- 4-api/solutions/4-4-1.py
+++ 4-api/solutions/4-4-1.py
@@ -6,7 +6,26 @@ import pandas as pd
 import numpy as np
 import json
 
+from streamlit import code
+
 app = FastAPI()
 
 url = "https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/flights/sample-flights.csv"
 df =pd.read_csv(url)
+#print(df.head())
+# departure_airport_code
+# arrival_airport_code
+# KJP, FUN -> departure
+# VOG, POW -> arrival
+
+@app.get("/api/search_flights")
+def search_flights(type:str =Query(), code:str =Query()):
+    if type == "dep":
+        flights = df[df["departure_airport_code"] == code]
+    elif type == "arr":
+        flights = df[df["arrival_airport_code"] == code]
+    else:
+        return {"error": "Invalid type. should be 'dep' or 'arr'"}
+
+    json_flights = flights.to_json(orient="records")
+    return json.loads(json_flights) # python list of dictionaries
